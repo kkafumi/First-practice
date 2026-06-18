82f5605b6c9bbb4c3fcb7c687c35180f8c5e230b
kkafumi <langa.voda@gmail.com>
2026-06-18 13:41:13 +0800

    Task 2 is solved

--- second_task.cpp
+++ second_task.cpp
@@ -17,7 +17,7 @@ class Graph {
   std::vector<int> GetChildren(int vertex) {
     return tree_[vertex];
   }
-  void DFS(int start, std::vector<bool> used) {
+  void DFS(int start, std::vector<bool>& used) {
     used[start] = true;
     std::vector<int> children = GetChildren(start);
     for (int i = 0; i < static_cast<int>(children.size()); ++i) {
@@ -30,6 +30,26 @@ class Graph {
 };
 
 int main() {
-
+  int number_vertexes = 0;
+  int number_edges = 0;
+  std::cin >> number_vertexes;
+  std::cin >> number_edges;
+  std::vector<bool> used(number_vertexes, false);
+  Graph moon_system(number_vertexes);
+  for (int i = 0; i < number_edges; ++i) {
+    int from = 0;
+    int to = 0;
+    std::cin >> from;
+    std::cin >> to;
+    moon_system.AddEdge(from - 1, to - 1);
+  }
+  int count_components = 0;
+  for (int i = 0; i < number_vertexes; ++i) {
+    if (!used[i]) {
+      moon_system.DFS(i, used);
+      count_components += 1;
+    }
+  }
+  std::cout << "Number of required communication lines: " << count_components - 1 << "\n";
   return 0;
 }
