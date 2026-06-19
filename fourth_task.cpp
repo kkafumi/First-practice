022a1e4e4f619b228960fe31f8219f4c81794d9d
kkafumi <langa.voda@gmail.com>
2026-06-19 20:40:33 +0800

    first try solution task 4

---
+++ fourth_task.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+int main() {
+  int letters_number = 0;
+  int permutation_number = 0;
+  std::cin >> letters_number;
+  std::cin >> permutation_number;
+
+  std::vector<int> permutation(letters_number);
+  std::vector<char> word(letters_number);
+
+  for (int i = 0; i < letters_number; ++i) {
+    std::cin >> permutation[i];
+  }
+  for (int i = 0; i < letters_number; ++i) {
+    std::cin >> word[i];
+  }
+  std::vector<char> result(letters_number);
+  for (int i = 0; i < permutation_number; ++i) {
+    for (int i = 0; i < letters_number; ++i) {
+      result[i] = word[permutation[i]];
+    }
+    word = result;
+  }
+  for (auto& a : result) {
+    std::cout << a;
+  }
+  return 0;
+}
