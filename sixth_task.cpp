6dfa0aa41b3c4862b400b9369f36cac1b2ca76dc
kkafumi <langa.voda@gmail.com>
2026-06-22 22:41:16 +0800

    solving sixth task

---
+++ sixth_task.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+  int request_number = 0;
+  std::cin >> request_number;
+  for (int i = 0; i < request_number; ++i) {
+    int a_controller_number = 0;
+    int b_controller_number = 0;
+    int module_number_1 = 0;
+    int module_number_2 = 0;
+    std::cin >> a_controller_number >> b_controller_number >> module_number_1 >> module_number_2;
+
+    int b_1 = std::min(b_controller_number, module_number_1);
+    module_number_1 -= b_1;
+    int a_1 = std::min(a_controller_number, module_number_1);
+    a_controller_number -= a_1;
+    int a_2 = std::min(a_controller_number, module_number_2);
+    std::cout << b_1 + a_1 + a_2 << " ";
+  }
+  std::cout << "\n";
+
+  return 0;
+}
