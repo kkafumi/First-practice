7d1fe39b6848e022ece5d0eaa576e37b7b5ef4ae
kkafumi <langa.voda@gmail.com>
2026-06-22 22:24:33 +0800

    fixed solving fifth task

--- fifth_task.cpp
+++ fifth_task.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <vector>
 int main() {
   int height = 0;
   int width = 0;
   std::cin >> height;
   std::cin >> width;
 
-  int start_x = -1;
-  int start_y = -1;
+  int start_x = width;
+  int start_y = height;
   int finish_x = -1;
   int finish_y = -1;
 
@@ -15,17 +14,15 @@ int main() {
     for (int j = 0; j < width; ++j) {
       int value = 0;
       std::cin >> value;
-      if (value == 1 && start_x == -1) {
-        start_x = j - 1;
-        start_y = i - 1;
-      }
       if (value == 1) {
-        finish_x = j + 1;
-        finish_y = i + 1;
+        start_x = std::min(start_x, j);
+        start_y = std::min(start_y, i);
+        finish_x = std::max(finish_x, j);
+        finish_y = std::max(finish_y, i);
       }
     }
   }
 
-  std::cout << start_x << " " << start_y << " " << finish_x << " " << finish_y << "\n";
+  std::cout << start_y - 1 << " " << start_x - 1 << " " << finish_y + 1 << " " << finish_x + 1 << "\n";
   return 0;
 }
