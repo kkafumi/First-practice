1af25b724ad50afe99b2efe0ba179fad21295343
kkafumi <langa.voda@gmail.com>
2026-06-24 17:16:28 +0800

    first solving nine task

--- nine_task/sketch.ino
+++ nine_task/sketch.ino
@@ -1,11 +1,27 @@
+#define GREEN_PIN 5
+#define RED_PIN 9
+#define PHOTO_PIN A0
 
+const unsigned long OpenDuration = 4000;
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(RED_PIN, OUTPUT);
 
+  digitalWrite(GEEN_PIN, LOW);
+  digitalWrite(RED_PIN, HIGH);
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (analogRead(PHOTO_PIN) > 512) {
+    digitalWrite(RED_PIN, LOW);
+    digitalWrite(GREEN_PIN, HIGH);
+    Serial.println("DOOR IS OPEN");
+    delay(OpenDuration);
+  } else {
+    digitalWrite(GREEN_PIN, LOW);
+    digitalWrite(RED_PIN, HIGH);
+    Serial.println("DOOR IS CLOSED");
+  }
 }
