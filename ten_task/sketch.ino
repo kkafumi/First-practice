506830f14a34b117f41e41e866085dab548706d9
Kkafumi <langa.voda@gmail.com>
2026-06-26 23:45:13 +0800

    first sketch ten task

--- ten_task/sketch.ino
+++ ten_task/sketch.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+#define SERVO_PIN 3
+
+Servo servop;
+
+void setup() {
+  servop.attach(SERVO_PIN);
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(Serial.available()){
+    String str = Serial.readStringUntil('\n');
+    int number = str.toInt();
+    if (number >= 0 && number <= 180){
+      int current_angle = servop.read();
+      for(int i = current_angle; i <= number; ++i){
+        servop.write(i);
+        delay(15);
+      }
+    }
+  }
 }
